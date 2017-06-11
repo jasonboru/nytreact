ec82f3111be0aad4b8993d35a3f257e48b1891a4
jasonboru <jasonboru@gmail.com>
2017-06-11 17:48:56 -0400

    added attribute to add response into the Results component

--- app/components/main.js
+++ app/components/main.js
@@ -43,7 +43,7 @@ class Main extends Component {
 		return (
 			<div className="container">
 			  <Search resetRes={this.resetRes} setQuery={this.setQuery} setStart={this.setStart} setEnd={this.setEnd} />
-			  <Results />
+			  <Results res={this.state.res}/>
 			  <Saved />
 			</div>
 		)
